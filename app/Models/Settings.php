4e4740dd407da83e90c7261d0eff76d099d8fe24
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-06-28 18:25:49 +0300

    settings-table

---
+++ app/Models/Settings.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $key
+ * @property string $value
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Settings extends Model
+{
+    use HasFactory;
+
+    protected $table = 'settings';
+
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+
+    public static function get(string $key, ?string $default = null): ?string
+    {
+        return self::query()->where('key', $key)->first() ?? $default;
+    }
+
+    public static function set(string $key, string $value): bool
+    {
+        /** @var Settings $setting */
+        if ($setting = self::get($key)) {
+            $setting->value = $value;
+            return $setting->save();
+        }
+        return (bool)self::query()->create(['key' => $key, 'value' => $value]);
+    }
+
+    public static function exists(string $key): bool
+    {
+        return (bool)self::query()->where($key, 'key')->exists();
+    }
+}
