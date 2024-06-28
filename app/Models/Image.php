62ae0576a647e983c5b8f3a0d60440240c716e3a
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-06-28 17:27:14 +0300

    image-model

---
+++ app/Models/Image.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+
+/**
+ * @property int $id
+ * @property string $path
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Image extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'path',
+    ];
+
+    /**
+     * @param UploadedFile $file
+     * @param string $storagePath path from /storage/app/ folder
+     * @return Image
+     * @throws \Exception
+     */
+    public static function storeUploadedFile(UploadedFile $file, string $storagePath): self
+    {
+        $originalName = $file->getClientOriginalName();
+
+        if (!$file->storeAs($storagePath, $originalName)) {
+            throw new \Exception("File saving failed");
+        }
+
+        return self::createFromPath("app/$storagePath/$originalName");
+    }
+
+    public static function createFromPath(string $path): Image
+    {
+        /** @var Image */
+        return self::query()->create(['path' => $path]);
+    }
+
+    public function getPublicUrl(): string
+    {
+        return str_replace('app/public', 'storage', asset($this->path));
+    }
+}
