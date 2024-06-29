4333a86822f59fc3089f2e138715a0d4971e42af
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-06-29 18:41:34 +0300

    some-front

--- resources/views/components/user-nav-bar.blade.php
+++ resources/views/components/user-nav-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="nav">
     <button class="nav__search">
-        <img src="img/search-icon.svg" class="nav__search-icon" alt="search icon"/>
+        <img src="/img/search-icon.svg" class="nav__search-icon" alt="search icon"/>
     </button>
     <button class="nav__order button-text--orange">Заказать</button>
     <div class="nav__lang button-text">Ру</div>
